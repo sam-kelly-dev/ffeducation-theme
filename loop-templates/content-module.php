c6f7f2bdf29e5825c96f30f9b831563ff8c557b5
skelly-jbfc <skelly@burnsfilmcenter.org>
2020-09-29 15:45:32 -0400

    Change background-image-position for module pages

--- loop-templates/content-module.php
+++ loop-templates/content-module.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 
 <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<div style="background-image: url(<?php echo the_post_thumbnail_url( $post->ID, 'large'); ?>); background-size: cover; background-position: -10px 0px; height: 60vh;">
+	<div style="background-image: url(<?php echo the_post_thumbnail_url( $post->ID, 'large'); ?>); background-size: cover; background-position: 50% 50%; height: 60vh;">
 		<div class="container d-flex" style="height: 100%;">
 			<div class="row align-items-center text-white"><?php the_title( '<h1 class="entry-title"><strong>', '</strong></h1>' ); ?></div>
 		</div>
