809f654222295695234178f4764980bea3de9425
skelly-jbfc <skelly@burnsfilmcenter.org>
2021-04-02 08:53:01 -0400

    Tweaking stuff.

--- page-templates/curriculum-page-template.php
+++ page-templates/curriculum-page-template.php
@@ -42,6 +42,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 		</div><!-- .row end -->
 
 	</div><!-- #content -->
+
+	<!--
 	<div class="container-fluid film-footer">
 		<div class="row text-center text-white">
 			<div class="col">
@@ -72,7 +74,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			</div>
 		</div>
 	</div>
-
+	-->
 </div><!-- #full-width-page-wrapper -->
 
 <?php get_footer(); ?>
