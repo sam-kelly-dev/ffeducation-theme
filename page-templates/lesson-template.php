2f4ad576b9bbba1fc801f65d52a4009bfed8e67f
skelly-jbfc <skelly@burnsfilmcenter.org>
2021-04-02 09:14:50 -0400

    Category pull

--- page-templates/lesson-template.php
+++ page-templates/lesson-template.php
@@ -77,7 +77,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 	    				<!-- GET SIBLINGS -->
 	    				<?php
 	    					// get all the children
-	    					$args = ['post_type' => 'page', 'order' => 'ASC', 'orderby' => 'menu_order', 'category' => 'module'];
+	    					$args = [
+	    						'post_type' => 'page', 'order' => 'ASC', 'orderby' => 'menu_order', 
+				                'taxonomy' => 'category',
+				                        'field' => 'slug',
+				                        'term' => 'module'
+				                    ];
 	    					$children = get_pages($args);
 	    					$found_next_module = false;
 	    					foreach($children as $child) {
