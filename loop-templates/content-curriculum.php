780ffe8dccc172a4f280d7f47fc04a0d57982e17
skelly-jbfc <skelly@burnsfilmcenter.org>
2021-04-02 11:18:10 -0400

    Whatever

--- loop-templates/content-curriculum.php
+++ loop-templates/content-curriculum.php
@@ -12,7 +12,7 @@ $parent_id = $post->post_parent;
 $parent_post = get_post($post->post_parent);
 ?>
 
-<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<div <?php post_class(); ?> id="post-<?php the_ID(); ?>" style="min-height: 50vh;">
 
 	<div class="entry-content">
 		<!-- .entry-meta -->
